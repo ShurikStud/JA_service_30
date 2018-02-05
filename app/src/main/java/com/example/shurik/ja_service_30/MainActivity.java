f780b2e14d121d4f213f668f4a1d758ac0171855
note-5i\shurik <abashev.alexandr@gmail.com>
2018-02-05 20:41:09 +0200

    + вывод всех работающих сервисов

--- app/src/main/java/com/example/shurik/ja_service_30/MainActivity.java
+++ app/src/main/java/com/example/shurik/ja_service_30/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.shurik.ja_service_30;
 
+import android.app.ActivityManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ListPopupWindow;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.shurik.ja_service_30.service.CustomService;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     Button buttonStart, buttonStop;
 
@@ -22,6 +29,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         buttonStart.setOnClickListener(this);
         buttonStop.setOnClickListener(this);
+
+        getServices();
 
     }
 
@@ -45,4 +54,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
+    private void getServices(){
+
+        ActivityManager activityManager = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> rs = activityManager.getRunningServices(50);
+        Log.v(TAG, "RunningService size = " + rs.size());
+
+        for (int i = 0; i < rs.size(); i ++){
+
+            ActivityManager.RunningServiceInfo runningServiceInfo = rs.get(i);
+
+            Log.v(TAG, "Service process " + runningServiceInfo.process + " with component " + runningServiceInfo.service.getClassName());
+
+        }
+
+    }
+
 }
