e1cbb57867c2cda2b713d67722f80657ddfdf8f6
note-5i\shurik <abashev.alexandr@gmail.com>
2018-02-05 20:31:21 +0200

    первый рабочий сервис

--- app/src/main/java/com/example/shurik/ja_service_30/MainActivity.java
+++ app/src/main/java/com/example/shurik/ja_service_30/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.shurik.ja_service_30;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.shurik.ja_service_30.service.CustomService;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -29,10 +32,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case R.id.activity_main_button_start:
 
+                startService(new Intent(MainActivity.this, CustomService.class));
+
                 break;
 
             case R.id.activity_main_button_stop:
 
+                stopService(new Intent(MainActivity.this, CustomService.class));
 
                 break;
 
